b019188ed23fb0f0e38210fa1322f3805b43bec2
胡钰维 <huyuwei1996@gmail.com>
2019-11-12 12:33:20 +0800

    update 01

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+rm -f 1.log
 for dper in `cat src/dper.txt`;do
-python3 src/bawangcan.py ${dper} >>1.log;
+python3 src/bawangcan.py ${dper} #>>1.log;
+python3 src/bawangcan01.py ${dper} #>>1.log;
 done
